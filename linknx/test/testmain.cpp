2c604466a090439eb720b36499c0d4f4dc6f676b
jef2000 <jef2000>
2009-03-08 22:52:41 +0000

    Integrate logging config into XML framework

--- linknx/test/testmain.cpp
+++ linknx/test/testmain.cpp
@@ -5,7 +5,7 @@
 
 int main( int argc, char **argv)
 {
-  initLogging();
+  Logging::instance()->defaultConfig();
   CppUnit::TextUi::TestRunner runner;
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest( registry.makeTest() );
