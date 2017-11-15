17efcc741672717267487d46b56c155bba4c9543
Cyrille Defranoux <cyrille.github@youplaboum.fr>
2017-11-15 20:50:24 +0100

    Restored build system in the release branch.

---
+++ config.h
@@ -0,0 +1,245 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the `dup2' function. */
+#define HAVE_DUP2 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have a functional curl library. */
+/* #undef HAVE_LIBCURL */
+
+/* Build with libesmtp email support. */
+#define HAVE_LIBESMTP 1
+
+/* libesmtp is compiled with pthread support. */
+#define HAVE_LIBESMTP_PTHREAD 1
+
+/* liblog4cpp */
+/* #undef HAVE_LOG4CPP */
+
+/* liblua */
+/* #undef HAVE_LUA */
+
+/* lua.h */
+/* #undef HAVE_LUA_H */
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* mysql is available */
+/* #undef HAVE_MYSQL */
+
+/* Define to 1 if the system has the type `ptrdiff_t'. */
+#define HAVE_PTRDIFF_T 1
+
+/* Define to 1 if stdbool.h conforms to C99. */
+#define HAVE_STDBOOL_H 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if the tm_gmtoff member is present in struct tm. */
+#define HAVE_TM_GMTOFF 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vfork' function. */
+#define HAVE_VFORK 1
+
+/* Define to 1 if you have the <vfork.h> header file. */
+/* #undef HAVE_VFORK_H */
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Define to 1 if `fork' works. */
+#define HAVE_WORKING_FORK 1
+
+/* Define to 1 if `vfork' works. */
+#define HAVE_WORKING_VFORK 1
+
+/* Define to 1 if the system has the type `_Bool'. */
+#define HAVE__BOOL 1
+
+/* Define if the __tm_gmtoff member is present in struct tm. */
+/* #undef HAVE___TM_GMTOFF */
+
+/* Defined if libcurl supports AsynchDNS */
+/* #undef LIBCURL_FEATURE_ASYNCHDNS */
+
+/* Defined if libcurl supports IDN */
+/* #undef LIBCURL_FEATURE_IDN */
+
+/* Defined if libcurl supports IPv6 */
+/* #undef LIBCURL_FEATURE_IPV6 */
+
+/* Defined if libcurl supports KRB4 */
+/* #undef LIBCURL_FEATURE_KRB4 */
+
+/* Defined if libcurl supports libz */
+/* #undef LIBCURL_FEATURE_LIBZ */
+
+/* Defined if libcurl supports NTLM */
+/* #undef LIBCURL_FEATURE_NTLM */
+
+/* Defined if libcurl supports SSL */
+/* #undef LIBCURL_FEATURE_SSL */
+
+/* Defined if libcurl supports SSPI */
+/* #undef LIBCURL_FEATURE_SSPI */
+
+/* Defined if libcurl supports DICT */
+/* #undef LIBCURL_PROTOCOL_DICT */
+
+/* Defined if libcurl supports FILE */
+/* #undef LIBCURL_PROTOCOL_FILE */
+
+/* Defined if libcurl supports FTP */
+/* #undef LIBCURL_PROTOCOL_FTP */
+
+/* Defined if libcurl supports FTPS */
+/* #undef LIBCURL_PROTOCOL_FTPS */
+
+/* Defined if libcurl supports HTTP */
+/* #undef LIBCURL_PROTOCOL_HTTP */
+
+/* Defined if libcurl supports HTTPS */
+/* #undef LIBCURL_PROTOCOL_HTTPS */
+
+/* Defined if libcurl supports IMAP */
+/* #undef LIBCURL_PROTOCOL_IMAP */
+
+/* Defined if libcurl supports LDAP */
+/* #undef LIBCURL_PROTOCOL_LDAP */
+
+/* Defined if libcurl supports POP3 */
+/* #undef LIBCURL_PROTOCOL_POP3 */
+
+/* Defined if libcurl supports RTSP */
+/* #undef LIBCURL_PROTOCOL_RTSP */
+
+/* Defined if libcurl supports SMTP */
+/* #undef LIBCURL_PROTOCOL_SMTP */
+
+/* Defined if libcurl supports TELNET */
+/* #undef LIBCURL_PROTOCOL_TELNET */
+
+/* Defined if libcurl supports TFTP */
+/* #undef LIBCURL_PROTOCOL_TFTP */
+
+/* Name of package */
+#define PACKAGE "linknx"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "linknx@ouaye.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "linknx"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "linknx 0.0.1.34"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "linknx"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.1.34"
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* STL streams has support for 64 bit integer */
+#define STL_STREAM_SUPPORT_INT64 1
+
+/* Use TICPP interface */
+#define TIXML_USE_TICPP 1
+
+/* Version number of package */
+#define VERSION "0.0.1.34"
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define curl_free() as free() if our version of curl lacks curl_free. */
+/* #undef curl_free */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int16_t */
+
+/* Define to the type of a signed integer type of width exactly 64 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef int64_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
+
+/* Define as `fork' if `vfork' does not work. */
+/* #undef vfork */
